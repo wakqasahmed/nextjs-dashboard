693e8fc3baa2e1029846f843ac630f13dc70d5eb
Waqas Ahmed <john@doe.com>
2025-12-13 18:27:57 +0500

    Bismillah - The first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+const inter = Inter({ subsets: ['latin'] });
+
+const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin']});
+
+export { inter, lusitana};
